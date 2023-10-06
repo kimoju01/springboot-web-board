5f2be3355fef73348252a98b772c3b9fe86d7647
kimoju01 <kimoju03@naver.com>
2023-10-06 19:59:03 +0900

    Initial commit

---
+++ src/main/java/com/study/springbootwebboard/SpringbootWebBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbootwebboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springbootwebboard/SpringbootWebBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbootwebboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
