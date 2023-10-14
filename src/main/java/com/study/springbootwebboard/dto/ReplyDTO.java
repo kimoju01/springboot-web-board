e3984575a098f664aa866ecf5461f39472c30e52
kimoju01 <kimoju03@naver.com>
2023-10-14 18:27:35 +0900

    feat: 댓글 DTO 추가

---
+++ src/main/java/com/study/springbootwebboard/dto/ReplyDTO.java
@@ -0,0 +1,22 @@
+package com.study.springbootwebboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDTO {
+
+    private Long rno;
+    private Long bno;
+    private String replyText;
+    private String replyer;
+    private LocalDateTime regDate, modDate;
+
+}
