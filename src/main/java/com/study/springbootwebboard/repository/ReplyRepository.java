fb885e6e54774f22c581ef6afb4de027b2aeabfe
kimoju01 <kimoju03@naver.com>
2023-10-14 19:28:38 +0900

    feat: ReplyRepository 인터페이스 추가

---
+++ src/main/java/com/study/springbootwebboard/repository/ReplyRepository.java
@@ -0,0 +1,9 @@
+package com.study.springbootwebboard.repository;
+
+import com.study.springbootwebboard.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
