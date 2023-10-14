3a55119a593078a5f4f61fe731b2fb1f0bd4adb3
kimoju01 <kimoju03@naver.com>
2023-10-14 19:27:26 +0900

    feat: Reply 엔티티 추가

---
+++ src/main/java/com/study/springbootwebboard/domain/Reply.java
@@ -0,0 +1,28 @@
+package com.study.springbootwebboard.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "board")    // exclude 속성? board 필드를 toString() 결과에서 제외
+public class Reply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    // fetch? EAGER(즉시 로딩): 엔티티가 로드될 때 연관 엔티티도 함께 로드 -> 모든 데이터 즉시 가져오기 때문에 성능 이슈
+    // LAZY(지연 로딩): 연관 엔티티를 필요한 시점에 로드 -> 필요한 경우에만 불러오므로 성능 향상 가능
+    @ManyToOne(fetch = FetchType.LAZY)  // 댓글(N) : 게시물(1) 다대일 관계
+    private Board board;
+
+    private String replyText;
+
+    private String replyer;
+
+}
