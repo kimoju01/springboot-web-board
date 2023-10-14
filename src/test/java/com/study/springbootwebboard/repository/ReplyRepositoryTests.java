4b6a79ab069ce46590219986410cc1f1314d5a14
kimoju01 <kimoju03@naver.com>
2023-10-14 20:18:58 +0900

    test: 특정 게시물 댓글 목록 검색 테스트

--- src/test/java/com/study/springbootwebboard/repository/ReplyRepositoryTests.java
+++ src/test/java/com/study/springbootwebboard/repository/ReplyRepositoryTests.java
@@ -6,6 +6,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import javax.transaction.Transactional;
 
 @SpringBootTest
 @Log4j2
@@ -32,5 +38,20 @@ public class ReplyRepositoryTests {
         replyRepository.save(reply);
 
     }
+
+    @Test
+    public void testBoardReplies() {
+
+        Long bno = 200L;
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> {
+            log.info(reply);
+        });
+
+    }
+
 }
 
