d0793d36d8b4e3bf36a411380f09d714c24b1f16
kimoju01 <kimoju03@naver.com>
2023-10-15 17:50:51 +0900

    feat: ReplyService 댓글 조회/수정/삭제 기능 추가

--- src/main/java/com/study/springbootwebboard/service/ReplyService.java
+++ src/main/java/com/study/springbootwebboard/service/ReplyService.java
@@ -6,5 +6,8 @@ public interface ReplyService {
 
     // 댓글 등록
     Long register(ReplyDTO replyDTO);
+    ReplyDTO read(Long rno);
+    void modify(ReplyDTO replyDTO);
+    void remove(Long rno);
 
 }
--- src/main/java/com/study/springbootwebboard/service/ReplyServiceImpl.java
+++ src/main/java/com/study/springbootwebboard/service/ReplyServiceImpl.java
@@ -8,6 +8,8 @@ import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
@@ -22,6 +24,32 @@ public class ReplyServiceImpl implements ReplyService {
         Long rno = replyRepository.save(reply).getRno();
 
         return rno;
+    }
+
+    @Override
+    public ReplyDTO read(Long rno) {
+        Optional<Reply> result = replyRepository.findById(rno);
+        // 아래 코드 추가해주지 않으면 테스트 코드에서 없는 번호 넣어도 예외를 안 띄워서 테스트 성공 띄워버림 !!
+        // 추가해주어야 NoSuchElementException 예외 발생해서 잘못된걸 알 수 있다.
+        Reply reply = result.orElseThrow();
+
+        ReplyDTO replyDTO = modelMapper.map(result, ReplyDTO.class);
+
+        return replyDTO;
+    }
+
+    @Override
+    public void modify(ReplyDTO replyDTO) {
+        Optional<Reply> result = replyRepository.findById(replyDTO.getRno());
+        Reply reply = result.orElseThrow();
+
+        reply.changeText(replyDTO.getReplyText());  // 댓글 내용 수정
+        replyRepository.save(reply);
+    }
+
+    @Override
+    public void remove(Long rno) {
+        replyRepository.deleteById(rno);
     }
 
 }
