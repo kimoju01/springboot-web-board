1e8f1a89feba53ef3bdae428448496b389feafb8
kimoju01 <kimoju03@naver.com>
2023-10-16 21:30:42 +0900

    feat: 댓글 JSON으로 출력 시 포맷팅 지정

--- src/main/java/com/study/springbootwebboard/dto/ReplyDTO.java
+++ src/main/java/com/study/springbootwebboard/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package com.study.springbootwebboard.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,6 +28,10 @@ public class ReplyDTO {
     @NotEmpty
     private String replyer;
 
-    private LocalDateTime regDate, modDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")    // JSON 처리 시에 포맷팅 지정
+    private LocalDateTime regDate;
+    
+    @JsonIgnore     // 댓글 수정 시간은 화면에서 출력할 필요 없기 때문에 JSON으로 변환될 때 제외 
+    private LocalDateTime modDate;
 
 }
